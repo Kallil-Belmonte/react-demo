7f7c0656538b3d9cfaeb20620ee20f10309eae19
Kallil Belmonte <kallil.belmonte@gmail.com>
2018-12-31 17:34:39 -0200

    File updated.

--- src/core/Redux/Actions/Account.js
+++ src/core/Redux/Actions/Account.js
@@ -1,9 +1,9 @@
 import ACTION_TYPES from './ActionTypes';
 
 // EDIT ACCOUNT
-export const editAccount = (post) => {
+export const editAccount = (userData) => {
   return {
     type: ACTION_TYPES.EDIT_ACCOUNT,
-    payload: post
+    payload: userData
   };
 };
