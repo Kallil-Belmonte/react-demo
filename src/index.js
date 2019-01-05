9d3eaeee5607ae8921ce8cfc83160ad24ba6e108
Kallil <kallil.belmonte@gmail.com>
2019-01-04 22:46:31 -0200

    React updated.

--- src/index.js
+++ src/index.js
@@ -23,7 +23,11 @@ import 'jquery-mask-plugin/dist/jquery.mask.min.js';
 //====================
 import './index.css';
 import App from './app/App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
