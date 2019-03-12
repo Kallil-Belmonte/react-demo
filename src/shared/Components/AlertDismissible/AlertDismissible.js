a21ab5412cea2f876f3a2d861d94b3e418c5cc4f
Kallil Belmonte <kallil.belmonte@gmail.com>
2019-03-11 22:35:08 -0300

    File updated.

--- src/shared/Components/AlertDismissible/AlertDismissible.js
+++ src/shared/Components/AlertDismissible/AlertDismissible.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { Alert } from 'reactstrap';
 
 const AlertDismissible  = (props) => {
-
   return(
     <Alert color={props.color} isOpen={true} toggle={() => props.dismiss()}>
       {props.children}
