6465a41c9a49e825c101d21fd436f140844b5a5e
Kallil <kallil.belmonte@gmail.com>
2020-04-30 17:53:48 -0300

    Files updated.

--- src/pages/Auth/Register/Register.js
+++ src/pages/Auth/Register/Register.js
@@ -1,127 +1,46 @@
-import React, { Component } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
 
 import { Container, Row, Col } from 'react-bootstrap';
 
-import { MOCKY_INSTANCE, ENDPOINTS } from 'core/API/API';
-import * as actionCreators from 'core/Redux/Actions/ActionCreators';
 import * as Helpers from 'shared/Helpers';
-import Loader from 'shared/Components/Loader/Loader';
 import RegisterForm from 'pages/Auth/Register/RegisterForm/RegisterForm';
 import './Register.scss';
 
-const { auth } = ENDPOINTS;
+const { setPageTitle } = Helpers;
 
-class Register extends Component {
-  state = {
-    isLoading: false,
-    form: {
-      fieldsErrors: {
-        email: [],
-        password: [],
-      },
-    },
-  }
-
-  componentDidMount() {
-    console.log('Utilize o e-mail: demo@demo.com para ver os alertas de erro.');
-    Helpers.setPageTitle('Register');
-    this.redirectLoggedUser();
-  }
-
-
-  //==============================
-  // GENERAL METHODS
-  //==============================
-
+const Register = ({ history }) => {
   // REDIRECT LOGGED USER
-  redirectLoggedUser() {
+  const redirectLoggedUser = useCallback(() => {
     const authToken = sessionStorage.getItem('authTokenReactDemo') || localStorage.getItem('authTokenReactDemo');
     const expiredSession = new Date().getTime() > Date.parse(localStorage.getItem('expirationDateReactDemo'));
 
     if (authToken && !expiredSession) {
-      this.props.history.push('/');
+      history.push('/');
     }
-  }
+  }, [history]);
+
+  // LIFECYCLE HOOKS
+  useEffect(() => {
+    console.log('Utilize o e-mail: demo@demo.com para ver os alertas de erro.');
+    setPageTitle('Login');
+    redirectLoggedUser();
+  }, []); // eslint-disable-line
+
+  return (
+    <main data-component="Register">
+      <Container>
+        <img src="/assets/img/logo.svg" className="logo d-block mx-auto" alt="logo" />
+
+        <Row>
+          <Col md={{ span: 6, offset: 3 }}>
+            <RegisterForm />
+        </Col>
+        </Row>
+      </Container>
+    </main>
+  );
+};
 
 
-  // HANDLE REGISTER
-  async handleRegister(values) {
-    this.setState({ isLoading: true });
-
-    try {
-      const { data } = await MOCKY_INSTANCE.post(auth.register, values);
-      const { token, firstName, lastName, email } = data;
-
-      this.setState({ isLoading: false });
-
-      if (values.email === 'demo@demo.com') {
-        this.setState((prevState, props) => ({
-          form: {
-            ...prevState.form,
-            fieldsErrors: {
-              ...prevState.form.fieldsErrors,
-              email: ['This e-mail already exists.'],
-              password: ['Your password is too weak.'],
-            },
-          },
-        }));
-      } else {
-        sessionStorage.setItem('authTokenReactDemo', token);
-        this.props.dispatchSetUserData({ firstName, lastName, email });
-        this.props.history.push('/');
-      }
-    } catch (error) {
-      console.error(error);
-      this.setState({ isLoading: false });
-    }
-  }
-
-
-  // HANDLE CLEAR FORM MESSAGE
-  handleClearFormMessage(object, property, index) {
-    // Helpers.clearFieldErrorMessage(this, 'form', object, property, index);
-  }
-
-
-  //==============================
-  // VIEW
-  //==============================
-
-  render() {
-    const { isLoading, form } = this.state;
-
-    return (
-      <main data-component="Register">
-        <Loader isLoading={isLoading} />
-
-        <Container>
-          <img src="/assets/img/logo.svg" className="logo d-block mx-auto" alt="logo" />
-
-          <Row>
-            <Col md={{ span: 6, offset: 3 }}>
-              <RegisterForm
-                fieldsErrors={form.fieldsErrors}
-                onClearFormMessage={(object, property, index) => this.handleClearFormMessage(object, property, index)}
-                onSubmit={(values) => this.handleRegister(values)}
-              />
-          </Col>
-          </Row>
-        </Container>
-      </main>
-    );
-  }
-}
-
-
-//==============================
-// REDUX
-//==============================
-
-// MAP DISPATCH TO PROPS
-const mapDispatchToProps = (dispatch) => ({
-  dispatchSetUserData: (userData) => dispatch(actionCreators.logIn(userData)),
-});
-
-export default connect(null, mapDispatchToProps)(withRouter(Register));
+export default withRouter(Register);
--- src/pages/Auth/Register/RegisterForm/RegisterForm.js
+++ src/pages/Auth/Register/RegisterForm/RegisterForm.js
@@ -1,86 +1,175 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, { Fragment, useReducer, useCallback } from 'react';
+import { NavLink, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
-import { Field, reduxForm } from 'redux-form';
 import { Form, Alert, Button } from 'react-bootstrap';
+import { useForm } from 'react-hook-form';
 
-import Validations from 'core/Forms/Validations';
-import { renderInput } from 'core/Forms/Fields';
+import { MOCKY_INSTANCE, ENDPOINTS } from 'core/API/API';
+import * as actionCreators from 'core/Redux/Actions/ActionCreators';
+import Reducer from 'core/Hooks/Reducer';
+import { emailPattern } from 'shared/Files/Regex';
+import * as Helpers from 'shared/Helpers';
+import Loader from 'shared/Components/Loader/Loader';
 import './RegisterForm.scss';
 
+const { auth } = ENDPOINTS;
+const { setFieldClassName, getFieldErrorMessage, removeItemsFromArray } = Helpers;
 const { Group, Label } = Form;
 
-const RegisterForm = ({
-  fieldsErrors,
-  onClearFormMessage,
-  handleSubmit,
-  valid,
-  pristine,
-  submitting,
-}) => {
+const initialState = {
+  isLoading: false,
+  emailErrors: [],
+  passwordErrors: [],
+};
+
+const RegisterForm = ({ history, dispatchSetUserData }) => {
+  const { register, formState, errors, handleSubmit } = useForm();
+  const { dirty, isSubmitting } = formState;
+
+  const [state, setState] = useReducer(Reducer, initialState);
+  const { isLoading, emailErrors, passwordErrors } = state;
+
+  // HANDLE REGISTER
+  const handleRegister = useCallback(async (values) => {
+    setState({ isLoading: true });
+
+    try {
+      const { data } = await MOCKY_INSTANCE.post(auth.register, values);
+      const { token, firstName, lastName, email } = data;
+
+      setState({ isLoading: false });
+
+      if (values.email === 'demo@demo.com') {
+        setState({
+          emailErrors: ['This e-mail does not exists.'],
+          passwordErrors: ['The password is incorrect.'],
+        });
+      } else {
+        sessionStorage.setItem('authTokenReactDemo', token);
+        dispatchSetUserData({ firstName, lastName, email });
+        history.push('/');
+      }
+    } catch (error) {
+      console.error(error);
+      setState({ isLoading: false });
+    }
+  }, [history]); // eslint-disable-line
+
+  // HANDLE CLEAR FORM MESSAGE
+  const handleClearFormMessage = useCallback((fieldName, fieldValue, index) => {
+    setState({ [fieldName]: removeItemsFromArray(true, fieldValue, [index]) });
+  }, []);
+
   return (
-    <Form data-component="RegisterForm" onSubmit={handleSubmit}>
-      <h1 className="page-title">Register</h1>
+    <Fragment>
+      <Loader isLoading={isLoading} />
 
-      <Group controlId="first-name">
-        <Label>First name</Label>
-        <Field id="first-name" className="form-control" component={renderInput} type="text" name="firstName" validate={[Validations.required]} />
-      </Group>
+      <Form data-component="RegisterForm" onSubmit={handleSubmit(handleRegister)}>
+        <h1 className="page-title">Register</h1>
 
-      <Group controlId="last-name">
-        <Label>Last name</Label>
-        <Field id="last-name" className="form-control" component={renderInput} type="text" name="lastName" validate={[Validations.required]} />
-      </Group>
+        <Group controlId="first-name">
+          <Label>First name</Label>
+          <input
+            id="first-name"
+            className={setFieldClassName(errors.firstName)}
+            type="text"
+            name="firstName"
+            ref={register({ required: { value: true, message: 'First name is required' } })}
+          />
+          {getFieldErrorMessage(errors.firstName)}
+        </Group>
 
-      <Group controlId="email">
-        <Label>E-mail</Label>
-        <Field id="email" className="form-control" component={renderInput} type="text" name="email" validate={[Validations.required, Validations.email]} errors={fieldsErrors.email} />
+        <Group controlId="last-name">
+          <Label>Last name</Label>
+          <input
+            id="last-name"
+            className={setFieldClassName(errors.lastName)}
+            type="text"
+            name="lastName"
+            ref={register({ required: { value: true, message: 'Last name is required' } })}
+          />
+          {getFieldErrorMessage(errors.lastName)}
+        </Group>
 
-        {fieldsErrors.email.map((errorMessage, index) =>
-          <Alert
-            key={errorMessage}
-            variant="danger"
-            dismissible
-            onClose={() => onClearFormMessage('fieldsErrors', 'email', index)}
-          >
-            {errorMessage}
-          </Alert>
-        )}
-      </Group>
+        <Group controlId="email">
+          <Label>E-mail</Label>
+          <input
+            id="email"
+            className={setFieldClassName(errors.email)}
+            type="text"
+            name="email"
+            ref={register({
+              required: { value: true, message: 'E-mail is required' },
+              pattern: { value: emailPattern, message: 'Invalid e-mail' },
+            })}
+          />
+          {getFieldErrorMessage(errors.email)}
 
-      <Group controlId="password">
-        <Label>Password</Label>
-        <Field id="password" className="form-control" component={renderInput} type="password" name="password" validate={[Validations.required, Validations.minLength3]} errors={fieldsErrors.password} />
+          {emailErrors.map((errorMessage, index) =>
+            <Alert
+              key={errorMessage}
+              variant="danger"
+              dismissible
+              onClose={() => handleClearFormMessage('emailErrors', emailErrors, index)}
+            >
+              {errorMessage}
+            </Alert>
+          )}
+        </Group>
 
-        {fieldsErrors.password.map((errorMessage, index) =>
-          <Alert
-            key={errorMessage}
-            variant="danger"
-            dismissible
-            onClose={() => onClearFormMessage('fieldsErrors', 'password', index)}
-          >
-            {errorMessage}
-          </Alert>
-        )}
-      </Group>
+        <Group controlId="password">
+          <Label>Password</Label>
+          <input
+            id="password"
+            className={setFieldClassName(errors.password)}
+            type="password"
+            name="password"
+            ref={register({
+              required: { value: true, message: 'Password is required' },
+              minLength: { value: 3, message: 'Minimum 3 characters required' },
+            })}
+          />
+          {getFieldErrorMessage(errors.password)}
 
-      <Button
-        className="d-block mx-auto"
-        variant="primary"
-        type="submit"
-        disabled={!valid || pristine || submitting}
-      >
-        Register
-      </Button>
+          {passwordErrors.map((errorMessage, index) =>
+            <Alert
+              key={errorMessage}
+              variant="danger"
+              dismissible
+              onClose={() => handleClearFormMessage('passwordErrors', passwordErrors, index)}
+            >
+              {errorMessage}
+            </Alert>
+          )}
+        </Group>
 
-      <div className="text-center">
-        <hr className="mt-4" />
-        Aleady have an account? <NavLink to="/login">Login</NavLink>
-      </div>
-    </Form>
+        <Button
+          className="d-block mx-auto"
+          variant="primary"
+          type="submit"
+          disabled={!dirty || isSubmitting}
+        >
+          Register
+        </Button>
+
+        <div className="text-center">
+          <hr className="mt-4" />
+          Aleady have an account? <NavLink to="/login">Login</NavLink>
+        </div>
+      </Form>
+    </Fragment>
   );
 };
 
-export default reduxForm({
-  form: 'register',
-})(RegisterForm);
+
+//==============================
+// REDUX
+//==============================
+
+// MAP DISPATCH TO PROPS
+const mapDispatchToProps = (dispatch) => ({
+  dispatchSetUserData: (userData) => dispatch(actionCreators.logIn(userData)),
+});
+
+export default connect(null, mapDispatchToProps)(withRouter(RegisterForm));
