d0fd351332ce6299be28faa742c0eea3a343219a
Kallil <kallil.belmonte@gmail.com>
2021-10-13 14:54:41 -0300

    Files updated.

--- src/shared/components/AppIcon/AppIcon.tsx
+++ src/shared/components/AppIcon/AppIcon.tsx
@@ -1,26 +1,35 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { CSSProperties, useState, useCallback, useEffect } from 'react';
 
 type Props = {
   className?: string;
   icon: string;
-  width?: string;
-  height?: string;
+  size?: string;
   fill?: string;
 };
 
-export type IconProps = Omit<Props, 'icon'>;
+export type IconProps = {
+  className?: string;
+  style: CSSProperties;
+  fill?: string;
+};
 
 const AppIcon = (props: Props) => {
   const [iconComponent, setIconComponent] = useState(null);
 
   const setComponent = useCallback(async () => {
+    const moduleProps = {
+      className: props.className || '',
+      style: props.size ? { width: props.size, height: props.size } : {},
+      fill: props.fill || 'currentColor',
+    };
+
     try {
       const module = await import(`./Icons/App${props.icon}`);
-      setIconComponent(module.default(props));
+      setIconComponent(module.default(moduleProps));
     } catch (error) {
       console.error(error);
     }
-  }, []);
+  }, [props]);
 
   // LIFECYCLE HOOKS
   useEffect(() => {
