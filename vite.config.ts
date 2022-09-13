01368561bd4b9dadd5f2c9632f60b803bc92aff7
Kallil Belmonte <kallil.belmonte@gmail.com>
2022-09-13 20:21:49 -0300

    Files updated.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,13 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "src/assets/scss/helpers/_helpers.scss";`,
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: {
