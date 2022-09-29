e4c6efe722c70ece4a400a75422afe9805c8f3b7
Kallil Belmonte <kallil.belmonte@gmail.com>
2022-09-29 13:05:33 -0300

    File updated.

--- src/shared/helpers/auth/redirectLoggedUser.ts
+++ src/shared/helpers/auth/redirectLoggedUser.ts
@@ -1,17 +1,14 @@
 import { NavigateFunction } from 'react-router';
 
 import { AUTH_EXPIRATION_DATE_KEY } from '@/shared/files/consts';
-import getAuthToken from './getAuthToken';
+import isExpiredSession from './isExpiredSession';
 
 /**
  * @name redirectLoggedUser
  */
 
 const redirectLoggedUser = (navigate: NavigateFunction) => {
-  const expiredSession =
-    new Date().getTime() > Date.parse(localStorage.getItem(AUTH_EXPIRATION_DATE_KEY) || '');
-
-  if (getAuthToken() && !expiredSession) {
+  if (!isExpiredSession()) {
     navigate('/');
   }
 };
