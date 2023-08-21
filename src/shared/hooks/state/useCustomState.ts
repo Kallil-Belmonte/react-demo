da9cbe1b1bd6451b45856b18d899eea94404d91f
Kallil Belmonte <kallil.belmonte@gmail.com>
2023-08-21 14:16:49 -0300

    File updated.

--- src/shared/hooks/state/useCustomState.ts
+++ src/shared/hooks/state/useCustomState.ts
@@ -1,17 +1,30 @@
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 
 /**
  * @name useCustomState
  * @description Hook for state management.
  */
 
+
 type TUseCustomState<Type> = [Type, (modifiedProps: Partial<Type>) => void];
 
 const useCustomState = <Type>(initialState: Type): TUseCustomState<Type> => {
+  const [mounted, setMounted] = useState(true);
   const [customState, setCustomState] = useState<Type>(initialState);
 
-  const handleSetCustomState = useCallback((modifiedProps: Partial<Type>) => {
-    setCustomState(prevState => ({ ...prevState, ...modifiedProps }));
+  const handleSetCustomState = useCallback(
+    (modifiedProps: Partial<Type>) => {
+      if (mounted) setCustomState((prevState) => ({ ...prevState, ...modifiedProps }));
+    },
+    [mounted],
+  );
+
+  // LIFECYCLE HOOKS
+  useEffect(() => {
+    // Unmount
+    return () => {
+      setMounted(false);
+    };
   }, []);
 
   return [customState, handleSetCustomState];
