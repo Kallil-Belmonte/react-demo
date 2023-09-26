983d3485915b6a10e1b04a5949ee2c0c57951e29
Kallil Belmonte <kallil.belmonte@gmail.com>
2023-09-26 15:39:44 -0300

    File updated.

--- src/shared/files/validations.ts
+++ src/shared/files/validations.ts
@@ -1,11 +1,11 @@
-import { Config } from '@/shared/helpers/string/validate';
+import { ValidationConfig } from '@/shared/helpers/string/validate';
 
-export const required: Config = { required: { check: true } };
+export const required: ValidationConfig = { required: { check: true } };
 
-export const requiredEmail: Config = { ...required, email: { check: true } };
+export const requiredEmail: ValidationConfig = { ...required, email: { check: true } };
 
 export const requiredSelect = (value: string) => ({
   custom: { isValid: value !== 'select', message: 'Required value.' },
 });
 
-export const requiredMin = (min: number): Config => ({ ...required, min: { check: min } });
+export const requiredMin = (min: number): ValidationConfig => ({ ...required, min: { check: min } });
