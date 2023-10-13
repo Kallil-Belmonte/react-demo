7e55cbd289e11a29e94149c796aa583798ccd010
Kallil Belmonte <kallil.belmonte@gmail.com>
2023-10-13 19:09:22 -0300

    File updated.

--- src/core/services/auth/index.ts
+++ src/core/services/auth/index.ts
@@ -1,15 +1,15 @@
 import request from '@/core/services/_files/request';
-import type { RegisterUserPayload, RegisterUser, LoginUserPayload, LoginUser, User } from './types';
+import type { RegisterUserPayload, LoginUserPayload, LoggedUser, User } from './types';
 
 const { stringify } = JSON;
 
-export const registerUser = (body: RegisterUserPayload): Promise<RegisterUser> =>
+export const registerUser = (body: RegisterUserPayload): Promise<LoggedUser> =>
   request(`01051ce0-738b-4a05-972a-24a0be715d9d`, {
     method: 'POST',
     body: stringify(body),
   });
 
-export const loginUser = (body: LoginUserPayload): Promise<LoginUser> =>
+export const loginUser = (body: LoginUserPayload): Promise<LoggedUser> =>
   request(`01051ce0-738b-4a05-972a-24a0be715d9d`, {
     method: 'POST',
     body: stringify(body),
