98dce9c87f922223ee9300f832b7ffe3ae46895f
Kallil Belmonte <kallil.belmonte@gmail.com>
2024-10-27 16:19:32 -0300

    File updated.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import biome from 'vite-plugin-biome';
 import deno from '@deno/vite-plugin';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     host: true,
